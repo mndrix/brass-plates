5959393320d32d6800f19f6699f5cdadf054b3a1
Michael Hendricks <michael@ndrix.org>
2008-10-13 20:47:29 -0600

    Label some edges as places
    When thinking about the way that historical records traveled,
    sometimes it's useful to think of records as staying in a single
    geographic location.  Other times, it's useful to think of them as
    following a single person.  This new graph makes the distinction where
    it seems useful.

--- picture.pl
+++ picture.pl
@@ -3,8 +3,13 @@ use strict;
 use warnings;
 use GraphViz;
 
-my %label_options = (
+# different label styles
+my %person_label = (
     fontcolor => 'blue',
+    fontsize  => 10,
+);
+my %place_label = (
+    fontcolor => 'darkgreen',
     fontsize  => 10,
 );
 
@@ -13,49 +18,77 @@ $g->add_edge( 'Creation' => 'Babel' );
 $g->add_edge( 'Babel'    => 'Abraham leaves Egypt' );
 $g->add_edge( 'Babel'    => 'Limhites return to Zarahemla',
     label     => "Jaredite record\n(discovered by Limhites)",
-    %label_options,
+    %person_label,
 );
 $g->add_edge( 'Abraham leaves Egypt' => 'Lehi leaves Jerusalem' );
-$g->add_edge( 'Abraham leaves Egypt' => "Book of Abraham\ntranslated" );
+$g->add_edge(
+    'Abraham leaves Egypt' => "Book of Abraham\ntranslated",
+    label                  => 'Egypt',
+    %place_label,
+);
 $g->add_edge(
     'Zedekiah to Babylon' => 'Mosiah finds Zarahemla',
     label => "Mulekites\n(no records)",
     style => 'dashed',
-    %label_options,
+    %person_label,
 );
-$g->add_edge( 'Lehi leaves Jerusalem'    => 'Zedekiah to Babylon' );
+$g->add_edge(
+    'Lehi leaves Jerusalem' => 'Zedekiah to Babylon',
+    label                   => 'Jerusalem',
+    %place_label,
+);
 $g->add_edge(
     'Lehi leaves Jerusalem' => 'Nephi leaves Laman',
     label                   => 'Lehi, &co',
-    %label_options,
+    %person_label,
 );
 $g->add_edge( 'Zedekiah to Babylon'      => 'Zedekiah arrives in Babylon' );
 $g->add_edge( 'Nephi leaves Laman'       => 'Mosiah leaves Lehi-Nephi' );
 $g->add_edge(
     'Mosiah leaves Lehi-Nephi' => 'Mosiah finds Zarahemla',
     label                      => 'Mosiah',
-    %label_options,
+    %person_label,
 );
-$g->add_edge( 'Mosiah leaves Lehi-Nephi' => 'Alma leaves Lehi-Nephi' );
-$g->add_edge( 'Mosiah finds Zarahemla'   => 'Zeniff departs Zarahemla' );
-$g->add_edge( 'Zeniff departs Zarahemla' => 'Ammon leaves Zarahemla' );
+$g->add_edge(
+    'Mosiah leaves Lehi-Nephi' => 'Alma leaves Lehi-Nephi',
+    label                      => 'Lehi-Nephi',
+    %place_label,
+);
+$g->add_edge(
+    'Mosiah finds Zarahemla' => 'Zeniff departs Zarahemla',
+    label                    => 'Zarahemla',
+    %place_label,
+);
+$g->add_edge(
+    'Zeniff departs Zarahemla' => 'Ammon leaves Zarahemla',
+    label                      => 'Zarahemla',
+    %place_label,
+);
 $g->add_edge(
     'Zeniff departs Zarahemla' => 'Ammon finds the Limhites',
     label                      => 'Zeniff',
-    %label_options,
+    %person_label,
 );
 $g->add_edge(
     'Ammon leaves Zarahemla' => 'Ammon finds the Limhites',
     label                    => 'Ammon',
-    %label_options,
+    %person_label,
 );
-$g->add_edge( 'Ammon leaves Zarahemla'   => 'Limhites return to Zarahemla' );
+$g->add_edge(
+    'Ammon leaves Zarahemla'   => 'Limhites return to Zarahemla',
+    label                      => 'Zarahemla',
+    %place_label,
+);
 $g->add_edge( 'Ammon finds the Limhites' => 'Limhites return to Zarahemla' );
-$g->add_edge( 'Limhites return to Zarahemla' => 'Alma arrives in Zarahemla' );
+$g->add_edge(
+    'Limhites return to Zarahemla' => 'Alma arrives in Zarahemla',
+    label                      => 'Zarahemla',
+    %place_label,
+);
 $g->add_edge(
     'Alma leaves Lehi-Nephi' => 'Alma arrives in Zarahemla',
     label                    => 'Alma',
-    %label_options,
+    %person_label,
 );
 
 
