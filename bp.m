4e08abb3a5d529ffac1cda97f96b85c607c4070e
Michael Hendricks <michael@ndrix.org>
2011-12-27 14:57:17 -0700

    More powerful, modular command-line tool
    This now has subcommands for generating GraphViz and PNG output.  It
    can also combine those steps and automatically display the PNG image
    when it's done.  This is particularly handy for quick testing:
    
        mmc --make bp && ./bp view

--- bp.m
+++ bp.m
@@ -8,7 +8,41 @@
 
 :- import_module list, solutions, string.
 :- import_module events, people.
+
+% which commands do we allow?
+:- type command --->
+    dot;
+    png;
+    view;
+    help.
+:- pred command_name(command, string).
+:- mode command_name(     in,    out) is det.
+:- mode command_name(    out,     in) is semidet.
+:- mode command_name(    out,    out) is multi.
+command_name(dot, "dot").
+command_name(help, "help").
+command_name(png,  "png").
+command_name(view,  "view").
+:- pred command_description(command, string).
+:- mode command_description(     in,    out) is det.
+command_description(dot, "Generate a .dot file for GraphViz").
+command_description(png, "Generate an event graph image").
+command_description(help, "Display command help").
+command_description(view, "Display a graph").
+
 main(!IO) :-
+    command_line_arguments(Argv, !IO),
+    (
+    if   Name = head(Argv), command_name(Cmd,Name)
+    then do_command(Cmd, !IO)
+    else print("Unknown command\n", !IO),
+         do_command(help, !IO)
+    ).
+
+% Do the real work for a subcommand
+:- pred do_command(command::in, io::di, io::uo) is cc_multi.
+do_command(dot, !IO) :-
+
     % output dot file header
     tell("graph.dot", _, !IO),
     print("digraph events {\n", !IO),
@@ -24,6 +58,32 @@ main(!IO) :-
     % output dot file footer
     print("}\n", !IO),
     told(!IO).
+
+do_command(png, !IO) :-
+    do_command(dot, !IO),
+    call_system( "dot -Tpng -ograph.png graph.dot", _, !IO ).
+
+do_command(view, !IO) :-
+    do_command(png, !IO),
+    call_system( "open graph.png", _, !IO ).
+
+do_command(help, !IO) :-
+    print("Known commands are:\n", !IO ),
+    solutions( all_commands, Commands ),
+    print_commands(Commands, !IO).
+
+:- pred all_commands(command::out) is multi.
+all_commands(Command) :-
+    command_name(Command, _).
+
+:- pred print_commands(list(command)::in, io::di, io::uo) is det.
+print_commands([], !IO).
+print_commands([C|Cs], !IO) :-
+    command_name(C, Name),
+    command_description(C, Desc),
+    format("   %-9s%s\n", [s(Name), s(Desc)], !IO),
+    print_commands(Cs, !IO).
+
 
 :- pred event_description(string::out) is nondet.
 event_description(Desc) :-
