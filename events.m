879c2f25ec177c6fa91e1164f040161ac581fe2c
Michael Hendricks <michael@ndrix.org>
2011-12-27 13:48:56 -0700

    Generate dot file for displaying event graph
    Run the following command to generate a PNG showing events and their
    relation to each other:
    
        mmc --make events &&
        ./events &&
        dot -Tpng -ograph.png graph.dot &&
        open graph.png
    
    Viewing the PNG is a great way to locate errors.  Unconnected nodes are a sure
    sign that something is wrong.

--- events.m
+++ events.m
@@ -233,9 +233,22 @@ and_then( nephi_returns_to_brothers, brothers_return_to_valley_of_lemuel, [
 % -------------------- Main and Helpers --------------- %
 :- import_module solutions.
 main(!IO) :-
-    format("Lehi's events:\n", [], !IO),
-    unsorted_solutions( event_description, Descriptions ),
-    print_all(Descriptions, !IO).
+    % output dot file header
+    tell("graph.dot", _, !IO),
+    print("digraph events {\n", !IO),
+
+    % output dot file nodes
+    unsorted_solutions( event_description, Nodes ),
+    print_nodes(Nodes, !IO),
+
+    % output dot file edges
+    unsorted_solutions( event_edges, Edges ),
+    print_edges(Edges, !IO),
+
+    % output dot file footer
+    print("}\n", !IO),
+    told(!IO).
+
 :- pred event_description(string::out) is nondet.
 event_description(Desc) :-
     event(_,Details),
@@ -246,8 +259,20 @@ event_description(Desc) :-
     else Desc = "(unknown)"
     ).
 
-:- pred print_all(list(string)::in, io::di, io::uo) is det.
-print_all([], !IO).
-print_all([S|Ss], !IO) :-
-    format(" %s\n", [s(S)], !IO),
-    print_all(Ss, !IO).
+:- pred print_nodes(list(string)::in, io::di, io::uo) is det.
+print_nodes([], !IO).
+print_nodes([S|Ss], !IO) :-
+    format("    \"%s\";\n", [s(S)], !IO),
+    print_nodes(Ss, !IO).
+
+:- pred event_edges({string,string}::out) is nondet.
+event_edges({From,To}) :-
+    and_then(FromNode,ToNode,_),
+    event( FromNode, FromDetails ), member(what(From),FromDetails),
+    event( ToNode,   ToDetails ),   member(what(To),  ToDetails).
+
+:- pred print_edges(list({string,string})::in, io::di, io::uo) is det.
+print_edges([],!IO).
+print_edges([{From,To}|Es], !IO) :-
+    format("    \"%s\" -> \"%s\";\n", [s(From),s(To)], !IO ),
+    print_edges(Es, !IO).
